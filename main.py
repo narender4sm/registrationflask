4bfa4a0df07a5305cd02adf975739ab423a2519b
narender4sm <narender4sm@gmail.com>
2023-06-04 21:44:27 +0530

    6th commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, render_template, request
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
-    if request.method=='POST':
+    if request.method == 'POST':
         fullname = request.form['fullname']
         htnumber = request.form['htnumber']
         branch = request.form['branch']
@@ -18,5 +19,6 @@ def registration():
         return render_template('success.html')
     else:
         return render_template('registration.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
